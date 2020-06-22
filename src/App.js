cc3c640d03c90f59622d3a2cd616fa5dd9e74ff5
Muhammad Saqib <saqisaqi.sk@gmail.com>
2020-06-21 22:34:59 -0700

    basic asynchronous

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import './App.css';
-import Hello from './hello';
 import './index.css'
 
-function App({name,age}) {
-const [isLit, setLit] = React.useState(true);
-return <div className={`room ${isLit ? "lit" : "dark"}`}> Hello  <strong>{name} </strong> 
-<br/>
-<ul>
-<li>Your Name is : {name}</li>
-<li>Your age is : {age}</li>
-<li>You are computer scientist</li>
-</ul>
- <Hello firstName={name}/>
- <button onClick={() => setLit(!isLit)}>
-    flip
-  </button> <br/>
- 
+function App() {
+ function prepareFood(callback) {
+    setTimeout(()=>
+    {
+      console.log("prepare paratha");
+      callback("paratha is ready");
+
+    },10);
+  }
+  function prepareEgg(callback) {
+    setTimeout(()=>
+    {
+      console.log("prepare Amlet");
+      callback("Amlet is ready");
+
+    },20);
+  }
+return <div >
+ <h1>Asynchronous function example</h1>
+ {prepareFood(function (value)
+ {
+   console.log(value);
+   prepareEgg(function (value)
+   {
+     console.log(value);
+   }
+   );
+ })}
  </div>
 }
 
