0ca90d4f80a431482bd7b1b4839dc22afc671259
Muhammad Saqib <saqisaqi.sk@gmail.com>
2020-06-28 01:09:43 -0700

    created simple menu bar

--- src/App.js
+++ src/App.js
@@ -3,39 +3,39 @@ import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
-import { Button } from '@material-ui/core';
+import { Button,AppBar,Toolbar,IconButton,Typography } from '@material-ui/core';
 import Icon from '@material-ui/core/Icon';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function App() {
   const useStyles = makeStyles((theme) => ({
-    button: {
-      margin: theme.spacing(1),
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
     },
   }));
 
   const classes = useStyles();
 
 return <div >
-  <Grid container spacing={3}>
-  <Grid item xs={12}>
-      Hello world1
-  </Grid>
-  <Grid item xs={6}>
-      Hello world2
-  </Grid>
-  <Grid item xs={6}>
-      Hello world3
-  </Grid>
-  <Grid item xs={2} style={{backgroundColor:"red"}}>
-      Hello world5
-  </Grid>
-  <Grid item xs={8} style={{backgroundColor:"green"}}>
-      Hello world6
-  </Grid>
-  <Grid item xs={2} style={{backgroundColor:"blue"}}>
-      Hello world7
-  </Grid>
-  </Grid>
+  <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
  </div>
 }
 
