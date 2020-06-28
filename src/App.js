e965d514170d573d5f7bd34bbfb14c451be6505e
Muhammad Saqib <saqisaqi.sk@gmail.com>
2020-06-27 23:21:05 -0700

    first added Button (color primary /secondary)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import './App.css';
-import Hello from './hello';
-import './index.css'
+import { Button } from '@material-ui/core';
 
-function App({name,age}) {
-const [isLit, setLit] = React.useState(true);
-return <div className={`room ${isLit ? "lit" : "dark"}`}> Hello  <strong>{name} </strong> 
-<br/>
-<ul>
-<li>Your Name is : {name}</li>
-<li>Your age is : {age}</li>
-<li>You are computer scientist</li>
-</ul>
- <Hello firstName={name}/>
- <button onClick={() => setLit(!isLit)}>
-    flip
-  </button> <br/>
- 
+function App() {
+
+return <div >
+  Hello material ui <br/>
+  <Button variant="contained" color="secondary">Hello Kayani</Button>
  </div>
 }
 
