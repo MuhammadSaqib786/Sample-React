a2602e21cb18a075c9d3afa86eaf5ba487b77ea8
Muhammad Saqib <saqisaqi.sk@gmail.com>
2020-06-28 02:40:04 -0700

    created bar chart

--- src/App.js
+++ src/App.js
@@ -1,42 +1,33 @@
 import React from 'react';
+import {Bar} from 'react-chartjs-2';
 import './App.css';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
-import { Button,AppBar,Toolbar,IconButton,Typography } from '@material-ui/core';
-import Icon from '@material-ui/core/Icon';
-import MenuIcon from '@material-ui/icons/Menu';
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'Muhammad Saqib Progress BSCS',
+      backgroundColor: ['red','green','blue','yellow','purple','brown','pink'],
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: [65, 59, 80, 81, 56, 55, 30]
+    }
+  ]
+};
 
 function App() {
-  const useStyles = makeStyles((theme) => ({
-    root: {
-      flexGrow: 1,
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
-    title: {
-      flexGrow: 1,
-    },
-  }));
-
-  const classes = useStyles();
-
-return <div >
-  <div className={classes.root}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </div>
- </div>
+ 
+return <div>
+  <Bar
+          data={data}
+          width={100}
+          height={200}
+          options={{
+            maintainAspectRatio: false
+          }}
+        />
+</div>
 }
 
 export default App;
