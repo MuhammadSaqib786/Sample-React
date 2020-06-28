5184a163ac39638835215d8d10386fb64dc0d917
Muhammad Saqib <saqisaqi.sk@gmail.com>
2020-06-28 00:19:42 -0700

    simple grid

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import { Button } from '@material-ui/core';
 import Icon from '@material-ui/core/Icon';
 
@@ -14,18 +16,26 @@ function App() {
   const classes = useStyles();
 
 return <div >
-  Hello material ui <br/>
-  <Button variant="contained" color="secondary">Hello Kayani</Button>
- <br/>
- This is send button   <Button
-        variant="contained"
-        color="primary"
-        className={classes.button}
-        endIcon={<Icon>send</Icon>}
-      >
-        Send
-      </Button>
- 
+  <Grid container spacing={3}>
+  <Grid item xs={12}>
+      Hello world1
+  </Grid>
+  <Grid item xs={6}>
+      Hello world2
+  </Grid>
+  <Grid item xs={6}>
+      Hello world3
+  </Grid>
+  <Grid item xs={2} style={{backgroundColor:"red"}}>
+      Hello world5
+  </Grid>
+  <Grid item xs={8} style={{backgroundColor:"green"}}>
+      Hello world6
+  </Grid>
+  <Grid item xs={2} style={{backgroundColor:"blue"}}>
+      Hello world7
+  </Grid>
+  </Grid>
  </div>
 }
 
