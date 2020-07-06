add28833d33184bb6c7150e47da71f6abbe08dd1
Muhammad Saqib <saqisaqi.sk@gmail.com>
2020-07-05 22:39:32 -0700

    coding adding routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
-import './App.css';
-import Hello from './hello';
-import './index.css'
+import React from "react";
+import "./App.css";
+import "./index.css";
+import Home from "./Home";
+import About from "./About";
+import { Routes, Route } from "react-router-dom";
 
-function App({name,age}) {
-const [isLit, setLit] = React.useState(true);
-return <div className={`room ${isLit ? "lit" : "dark"}`}> Hello  <strong>{name} </strong> 
-<br/>
-<ul>
-<li>Your Name is : {name}</li>
-<li>Your age is : {age}</li>
-<li>You are computer scientist</li>
-</ul>
- <Hello firstName={name}/>
- <button onClick={() => setLit(!isLit)}>
-    flip
-  </button> <br/>
- 
- </div>
+function App() {
+  return (
+    <div>
+      <h1>Routes</h1>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,24 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import MediaCard from './MediaCard';
-import myimage from './saqi.jpg'
-import Temperature from './Temprature'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.render(
-    <div> <App name="saqib kayani" age={26}/>
-    <MediaCard title="Assignment # 2" body="This is another assignment that passes three arguments title ,body and image and displays in next page" url={myimage}/>
-    <br/>
-    <Temperature/>
-    </div>,
-    
-  
-  document.getElementById('root')
-);
+  <div>
+    {" "}
+    <Router>
+      <App />
+    </Router>
+  </div>,
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  document.getElementById("root")
+);
